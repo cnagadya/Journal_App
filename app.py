fb3c28f407652063bfc4c3cb216a4de6829c40ad
Christine R Nagadya <Christine R Nagadya>
2017-03-13 13:59:35 +0000

    Landing Page layout

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "This is the Journal app"
+    return render_template('index.html')
+    
 
 
 if __name__ == "__main__":
